53f48b0c095981ad943939e3f3fdf7d53b90290e
roy9527 <myroy7@163.com>
2014-08-01 01:40:44 +0800

    add new

---
+++ MapsDemo/src/com/test/mapsdemo/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.test.mapsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+}
